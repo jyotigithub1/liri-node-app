2547a83615b036e5cfd5a73189ee13e1432db389
Jyoti Das <bootcampatut@gmail.com>
2019-06-25 23:22:52 -0500

    Added the finised project

--- liri.js
+++ liri.js
@@ -14,11 +14,6 @@ var inquier = require('inquirer');
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 // log(spotify);
-// including omdb 
-// const omdb = new (require('omdbapi'))(keys.ombd);
-const omdb = new (require('omdbapi'))("3ce7d33b");
-
-
 // including chalk
 const chalk = require("chalk");
 // log("omdb:::"+JSON.stringify(Omdb));
@@ -43,7 +38,7 @@ inquier.prompt([
                 type: "list",
                 name: "options",
                 message: "Welcome" + "  " + inresponse.username + " " + "here are the options!!",
-                choices: ["Need Info on Movies", "Need Info on Concert", "Spotify the Song", "Do whatever u want"]
+                choices: ["Need Info on Movies", "Need Info on Concert", "Spotify the Song", "Do what u Says"]
 
             }
         ]).then(function (innerresponse) {
@@ -57,8 +52,8 @@ inquier.prompt([
                 case "Need Info on Movies":
                     movieInfo();
                     break;
-                case "Do whatever u want":
-                    doWhatInfo();
+                case "Do what u Says":
+                    dowhatInfo();
                     break;
             }
 
@@ -88,14 +83,10 @@ function bandinfo() {
                 }
                 else {
                     log(chalk.magenta("Here is the band info for" + " " + bandresponse.bandInfo));
-                    for (var i = 0; i < response.data.length; i++) {
-                        var dateandtime = moment(response.data[i].datetime, "YYYY-MM-DD'T'HH:mm:ss").format("ddd, MMMM Do,HH:mm");
-                        log(chalk.red("--------------------------------------------------------------"));
-                        log(chalk.blue("Venue Name : " + chalk.white(JSON.stringify(response.data[i].venue.name))));
-                        log(chalk.blue("Venue Location: " + chalk.white(JSON.stringify(response.data[i].venue.city + "," + " " + response.data[i].venue.region + "," + " " + response.data[i].venue.country))));
-                        log(chalk.blue("Venue Date and Time: " + chalk.white(JSON.stringify(dateandtime))));
-                        log(chalk.red("--------------------------------------------------------------"));
-                    }
+                    banddisplay(response);
+
+
+
                 }
 
             }
@@ -105,6 +96,15 @@ function bandinfo() {
     });
 
 }
+function banddisplay(response) {
+    var dateandtime = moment(response.data[0].datetime, "YYYY-MM-DD'T'HH:mm:ss").format("ddd, MMMM Do,HH:mm");
+    log(chalk.red("--------------------------------------------------------------"));
+    log(chalk.blue("Venue Name : " + chalk.white(JSON.stringify(response.data[0].venue.name))));
+    log(chalk.blue("Venue Location: " + chalk.white(JSON.stringify(response.data[0].venue.city + "," + " " + response.data[0].venue.region + "," + " " + response.data[0].venue.country))));
+    log(chalk.blue("Venue Date and Time: " + chalk.white(JSON.stringify(dateandtime))));
+    log(chalk.red("--------------------------------------------------------------"));
+}
+
 // function for Song Info
 function songInfo() {
     inquier.prompt([
@@ -127,16 +127,23 @@ function songInfo() {
                 return log('Error occurred: ' + err);
             }
             log(chalk.magenta("Here is the SongInfo info for the Song " + " " + chalk.red(songresponse.songInfo)));
-            log(chalk.red("--------------------------------------------------------------"));
-            log(chalk.blue("Artist: " + chalk.white(data.tracks.items[0].artists[0].name)));
-            log(chalk.blue("Song: " + chalk.white(data.tracks.items[0].album.name)));
-            log(chalk.blue("Want to Listen the song click here : " +chalk.white(data.tracks.items[0].album.external_urls.spotify)));
-            log(chalk.blue("Album Name: " + chalk.white(data.tracks.items[0].album.artists[0].name)));
-            log(chalk.red("--------------------------------------------------------------"));
+            songdisplay(data);
+
         });
+
+
 
     });
 
+}
+function songdisplay(data) {
+
+    log(chalk.red("--------------------------------------------------------------"));
+    log(chalk.blue("Artist: " + chalk.white(data.tracks.items[0].artists[0].name)));
+    log(chalk.blue("Song: " + chalk.white(data.tracks.items[0].album.name)));
+    log(chalk.blue("Want to Listen the song click here : " + chalk.white(data.tracks.items[0].album.external_urls.spotify)));
+    log(chalk.blue("Album Name: " + chalk.white(data.tracks.items[0].album.artists[0].name)));
+    log(chalk.red("--------------------------------------------------------------"));
 }
 // function for movie info
 function movieInfo() {
@@ -159,16 +166,7 @@ function movieInfo() {
                 }
                 else {
                     log(chalk.magenta("Here is the Movie  info for" + " " + movieresponse.movieInfo));
-                    log(chalk.red("--------------------------------------------------------------"));
-                    log(chalk.blue("Title of the Movie" +" "+ chalk.white((response.data.Title))));
-                    log(chalk.blue("Year of the Movie:" +" " + chalk.white((response.data.Year))));
-                    log(chalk.blue("Rated:"+" " + chalk.white((response.data.imdbRating))));
-                    log(chalk.blue("Rotten Tomatoes Rating of the movie: " + chalk.white((response.data.Ratings[1].Value))));
-                    log(chalk.blue("Country where the movie was produced: " + chalk.white((response.data.Country))));
-                    log(chalk.blue("Language of the movie: " + chalk.white((response.data.Language))));
-                    log(chalk.blue("Plot of the movie: "+"\n" + chalk.white((response.data.Plot))));
-                    log(chalk.blue("Actors in the movie: " + chalk.white((response.data.Actors))));
-                    log(chalk.red("--------------------------------------------------------------"));
+                    moviedisplay(response);
 
 
                 }
@@ -180,9 +178,98 @@ function movieInfo() {
     });
 
 }
-function dowhatInfo() { }
+function moviedisplay(response) {
+    log(chalk.red("--------------------------------------------------------------"));
+    log(chalk.blue("Title of the Movie" + " " + chalk.white((response.data.Title))));
+    log(chalk.blue("Year of the Movie:" + " " + chalk.white((response.data.Year))));
+    log(chalk.blue("Rated:" + " " + chalk.white((response.data.imdbRating))));
+    log(chalk.blue("Rotten Tomatoes Rating of the movie: " + chalk.white((response.data.Ratings[1].Value))));
+    log(chalk.blue("Country where the movie was produced: " + chalk.white((response.data.Country))));
+    log(chalk.blue("Language of the movie: " + chalk.white((response.data.Language))));
+    log(chalk.blue("Plot of the movie: " + "\n" + chalk.white((response.data.Plot))));
+    log(chalk.blue("Actors in the movie: " + chalk.white((response.data.Actors))));
+    log(chalk.red("--------------------------------------------------------------"));
 
 
+}
+
+function dowhatInfo() {
+
+    fs.readFile("random.txt", "utf8", function (err, data) {
+
+        // If an error was experienced we will log it.
+        if (err) {
+            console.log(err);
+        }
+
+        // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+        else {
+            var output = data.split(",");
+            var userinput = output[0];
+            var name = output[1];
+            var equalstring = "spotify-this-song";
+            console.log(userinput);
+            if (output[0] === equalstring) {
+                spotify.search({ type: 'track', query: name, limit: 10 }, function (err, data) {
+
+                    // log(JSON.stringify(data, null, 2));
+                    if (err) {
+                        return log('Error occurred: ' + err);
+                    }
+                    songdisplay(data);
+                });
+            }
+            else if (output[2] === "Need Info on Movies") {
+                name = output[3];
+                console.log(output[2]);
+                var queryUrl = "http://www.omdbapi.com/?t=" + name + "&y=&plot=short&apikey=trilogy";
+                debugger;
+
+                axios.get(queryUrl).then(
+                    function (response) {
+                        if (!response) {
+                            log(chalk.yellow("--------------------------------------------------------------"));
+                            log(chalk.magenta("NO DATA FOUND..!!"));
+                            log(chalk.yellow("--------------------------------------------------------------"));
+                        }
+                        else {
+                            moviedisplay(response);
+                        }
+
+                    });
+            }
+            else if (output[4] === "Need Info on Concert") {
+                name = output[5];
+                console.log(output[4]);
+                var queryUrl = "https://rest.bandsintown.com/artists/" + name + "/events?app_id=codingbootcamp";
+                // https://rest.bandsintown.com/artists/Morrissey/events?app_id=codingbootcamp
+                axios.get(queryUrl).then(
+                    function (response) {
+                        if (!response) {
+                            log(chalk.yellow("--------------------------------------------------------------"));
+                            log(chalk.magenta("NO DATA FOUND..!!"));
+                            log(chalk.yellow("--------------------------------------------------------------"));
+                        }
+                        else {
+                            banddisplay(response);
+                        }
+                    });
+            }
+
+            else {
+                console.log("No Data Available");
+            }
+
+
+
+
+
+
+
+
+        }
+    });
+}
 
 
 
